b93452c67085996a53d099155f3235cf5b0c72af
MAKOSCAFEE <barnabasmakonda@gmail.com>
2020-04-02 14:16:40 +0200

    added api for operators

---
+++ pkg/apis/mongodboperator/group.go
@@ -0,0 +1,6 @@
+// Package mongodboperator contains mongodboperator API versions.
+//
+// This file ensures Go source parsers acknowledge the mongodboperator package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package mongodboperator
